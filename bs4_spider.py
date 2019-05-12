b73e05af4b8538abd9a372bd19f2c0ef7dd50d75
CasPaPaPa <zeonphantom@163.com>
2019-05-12 14:25:42 +0800

    update

--- bs4_spider.py
+++ bs4_spider.py
@@ -1,3 +1,6 @@
+# 参考资料：
+# requests中文文档：https://2.python-requests.org//zh_CN/latest/user/quickstart.html
+# BeautifulSoup4中文文档：https://www.crummy.com/software/BeautifulSoup/bs4/doc/index.zh.html
 import requests
 from bs4 import BeautifulSoup
 
@@ -6,7 +9,7 @@ url = "https://nba.hupu.com/"
 # 调用 requests 库中的 get() 函数，从目标地址中获取 Response
 page = requests.get(url, headers={'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'})
 
-# （可选）打印出 Response 中的网页内容（content）
+# （可选）打印出 Response 中的网页文档内容
 # print(page.text)
 
 soup = BeautifulSoup(page.text, "html.parser")
